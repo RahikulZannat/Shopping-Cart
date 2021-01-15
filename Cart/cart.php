738ba2fb14d218d841b80f0862c45eff6b9f7163
Rahikul Zannat <60150817+RahikulZannat@users.noreply.github.com>
2021-01-15 08:37:51 +0600

    Add files via upload

---
+++ Cart/cart.php
@@ -0,0 +1,133 @@
+<?php
+
+    session_start();
+    $database_name = "Product_details";
+    $con = mysqli_connect("localhost","root","",$database_name);
+
+    
+    if (isset($_POST["add"])){
+        if (isset($_SESSION["cart"])){
+            $item_array_id = array_column($_SESSION["cart"],"product_id");
+            if (!in_array($_GET["id"],$item_array_id)){
+                $count = count($_SESSION["cart"]);
+                $item_array = array(
+                    'product_id' => $_GET["id"],
+                    'item_name' => $_POST["hidden_name"],
+                    'product_price' => $_POST["hidden_price"],
+                    'item_quantity' => $_POST["quantity"],
+                );
+                $_SESSION["cart"][$count] = $item_array;
+                echo '<script>window.location="Cart.php"</script>';
+            }
+        }else{
+            $item_array = array(
+                'product_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'product_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["cart"][0] = $item_array;
+        }
+    }
+
+    if (isset($_GET["action"])){
+        if ($_GET["action"] == "delete"){
+            foreach ($_SESSION["cart"] as $keys => $value){
+                if ($value["product_id"] == $_GET["id"]){
+                    unset($_SESSION["cart"][$keys]);
+                    
+                }
+            }
+        }
+    }
+?>
+<!doctype html>
+<html>
+<head>
+    
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+
+</head>
+<body>
+
+    <div class="container">
+        <h2>Shopping Cart</h2>
+        <?php
+            $query = "SELECT * FROM product ORDER BY id ASC ";
+            $result = mysqli_query($con,$query);
+            if(mysqli_num_rows($result) > 0) {
+
+                while ($row = mysqli_fetch_array($result)) {
+
+                    ?>
+                    <div class="col-md-3">
+
+                        <form method="post" action="Cart.php?action=add&id=<?php echo $row["id"]; ?>">
+
+                            <div class="product">
+                                <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+                                <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
+                                <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                                <input type="text" name="quantity" class="form-control" value="1">
+                                <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
+                                <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                                <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                                       value="Add to Cart">
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                }
+            }
+        ?>
+
+        <div style="clear: both"></div>
+        <h3 class="title2">Shopping Cart Details</h3>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+            <tr>
+                <th width="30%">Product Name</th>
+                <th width="10%">Quantity</th>
+                <th width="13%">Price Details</th>
+                <th width="10%">Total Price</th>
+                <th width="17%">Remove Item</th>
+            </tr>
+
+            <?php
+                if(!empty($_SESSION["cart"])){
+                    $total = 0;
+                    foreach ($_SESSION["cart"] as $key => $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value["item_name"]; ?></td>
+                            <td><?php echo $value["item_quantity"]; ?></td>
+                            <td>$ <?php echo $value["product_price"]; ?></td>
+                            <td>
+                                $ <?php echo number_format($value["item_quantity"] * $value["product_price"], 2); ?></td>
+                            <td><a href="Cart.php?action=delete&id=<?php echo $value["product_id"]; ?>"><span
+                                        class="text-danger">Remove Item</span></a></td>
+
+                        </tr>
+                        <?php
+                        $total = $total + ($value["item_quantity"] * $value["product_price"]);
+                    }
+                        ?>
+                        <tr>
+                            <td colspan="3" align="right">Total</td>
+                            <th align="right">$ <?php echo number_format($total, 2); ?></th>
+                            <td></td>
+                        </tr>
+                        <?php
+                    }
+                ?>
+            </table>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
---
+++ Cart/product.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 15, 2021 at 03:12 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product_details`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(10) NOT NULL,
+  `pname` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `price` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `pname`, `image`, `price`) VALUES
+(1, 'iPhone 10 XS', '10xs.png', '549.99'),
+(2, 'iPhone 11 Pro Max', '11pm.png', '999'),
+(3, 'iPhone 12', '12.png', '949');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
